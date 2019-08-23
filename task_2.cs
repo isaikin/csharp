31b7d172026b0cf2e01de9a768298a0b0dad770e
kristina15 <54286198+kristina15@users.noreply.github.com>
2019-08-23 18:04:33 +0400

    Add files via upload

---
+++ task_2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("N= ");
+            string str = Console.ReadLine();
+            int N = int.Parse(str);
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = N; j >= N-(2*i); j-- )
+                    Console.Write("*");
+                   Console.WriteLine();
+               
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ task_3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("N= ");
+            string str = Console.ReadLine();
+            int N = int.Parse(str);
+            for (int i=0; i<N; i++)
+            {
+                for (int j = i; j < N; j++)
+                    Console.Write(" ");
+                for (int k = N; k >= N - (2 * i); k--)
+                    Console.Write("*");
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
+        }
+    }
+}
