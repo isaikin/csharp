7864ed75e14f23b83dd22db26366bbd26637fc7f
kristina15 <54286198+kristina15@users.noreply.github.com>
2019-08-26 02:19:59 +0400

    Add files via upload

---
+++ task_10.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[5, 5];
+            
+            int s = 0;
+            for (int i=0; i<arr.GetLength(0); i++)
+            {
+                for (int j=0; j<arr.GetLength(1); j++)
+                {
+                    Console.Write("Введите arr[{0}, {1}]=", i, j);
+                    arr[i, j] = int.Parse(Console.ReadLine());
+                   
+                    
+                }
+               
+            }
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                        if ((i%2 ==0 &&  j%2==0) || (i % 2 != 0 && j % 2 != 0))
+                        s += arr[i, j];
+
+                }
+
+            }
+            Console.WriteLine("Сумма равна:"+s);
+            Console.ReadLine();
+        }
+    }
+}
