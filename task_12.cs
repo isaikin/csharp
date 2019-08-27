ee00979e915d8fe463624e2e0226c769b918fe2b
kristina15 <54286198+kristina15@users.noreply.github.com>
2019-08-27 23:51:34 +0400

    Create task_12.cs

---
+++ task_12.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s= Console.ReadLine();
+            char[] ca = s.ToCharArray();
+            string m = Console.ReadLine();
+            char[] ma = m.ToCharArray();
+            for (int i = 0; i < ca.Length; i++)
+            {
+                 Console.Write(ca[i]);
+                for(int j = 0; j<ma.Length; j++)
+                {
+                    if (ca[i] == ma[j])
+                    {
+                       Console.Write(ca[i]);
+                    }
+                 }
+            }
+            Console.ReadLine();
+        }
+    }
+}
