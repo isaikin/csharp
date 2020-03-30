75a14aae178ba112cdbfc6ada57193b0cdd3cf77
kristina15 <54286198+kristina15@users.noreply.github.com>
2020-03-30 19:12:37 +0400

    Update task_11.cs

--- task_11.cs
+++ task_11.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace task_11
 {
@@ -10,14 +11,22 @@ namespace task_11
     {
         static void Main(string[] args)
         {
-            string m=Console.ReadLine();
-              string[] ar = { ",", ".", "!", "?", "-", " ", "^", ":" };
-              for (int i = 0; i < ar.Length; i++)
-              {
-                  m = m.Replace(ar[i], "");
-              }
-                     
-            Console.WriteLine("Длина строки:" + m.Length);
+            string m = Console.ReadLine();
+            int s = 0, k=0;
+            for (int i = 0; i < m.Length; i++)
+            {
+                if (char.IsLetter(m[i]))
+                {
+                    s++;
+                }
+                if((char.IsWhiteSpace(m[i]) && char.IsLetter(m[i+1])) || (i==m.Length-1 && char.IsLetter(m[i])) || (char.IsWhiteSpace(m[i]) && char.IsLetter(m[i - 1])))
+                k++;
+
+            }
+            if(k==0)
+                Console.WriteLine("Средняя длина слова: 0");
+            else
+            Console.WriteLine("Средняя длина слова:" + s/k);
             Console.ReadLine();
         }
     }
