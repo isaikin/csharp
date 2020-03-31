c388237d1cd3ed54a1d117a4e428dc926f31e876
kristina15 <54286198+kristina15@users.noreply.github.com>
2020-03-31 16:05:07 +0400

    Add files via upload

---
+++ task5.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskForYou
+{
+    class task5
+    {
+        public void SummaVseh()
+        {
+            int s = 0;
+            for (int i = 0; i < 1001; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                    s += i;
+            }
+            Console.Write(s);
+        }
+    }
+}
